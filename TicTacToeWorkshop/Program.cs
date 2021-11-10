5172fab2897273074a55f7b8c849a68afed95e28
Subhadeep <subhadeep0326@gmail.com>
2021-11-10 11:53:42 +0530

    Subhadeep added the TicTacToe game first UC

---
+++ TicTacToeWorkshop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToeWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Welcome to The tic tac toe board game!-------");
+            TicTacToeGame game = new TicTacToeGame();
+            game.DisplayBoardUC1();
+        }
+    }
+}
---
+++ TicTacToeWorkshop/TicTacToeGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeWorkshop
+{
+    public class TicTacToeGame
+    {
+        //declaring a char data type array of size 10 to accomodate all the 9 places in the game these are set to be empty.
+        char[] choice = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        
+        public void DisplayBoardUC1()
+        {
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", choice[1], choice[2], choice[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", choice[4], choice[5], choice[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", choice[7], choice[8], choice[9]);
+
+            Console.WriteLine("     |     |      ");
+        }
+    }
+}
